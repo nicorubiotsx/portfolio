4fc40e3a748430944990d751c225f69d59d6e80a
nicorubiotsx <nrubioolivares@gmail.com>
2024-09-06 11:30:13 -0400

    first commit initial base

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,113 @@
+'use client'
 import Image from "next/image";
-
+import x from '../img-profile.jpg'
+import { useEffect } from "react";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+  const empleos = [
+    {
+      empresa: 'Aidprof',
+      subtitulo: 'En esta compañía, participé activamente en el desarrollo de una aplicación móvil. A continuación, detallo los trabajos que realicé',
+      acciones: ['Diseño de la base de datos relacional: Creación y estructuración de la base de datos para garantizar la integridad y eficiencia en el manejo de datos', 'Configuración inicial del proyecto en Django: Configuración del entorno de desarrollo y estructura base del proyecto utilizando Django para sentar las bases del desarrollo', 'Creación de modelos en Django: Desarrollo de modelos en Django para definir la estructura de los datos y sus relaciones dentro de la aplicación', 'Desarrollo de endpoints: Implementación de endpoints para permitir la interacción entre el cliente y el servidor, facilitando la comunicación y el intercambio de datos']
+    },
+    {
+      empresa: 'Stage On Vr',
+      subtitulo: '',
+      acciones: ['Creación de nuevas pantallas para mejorar la experiencia del usuario', 'Gestión del estado global de la app utilizando Redux para asegurar un flujo de datos eficiente', 'Incorporación de notificaciones push (firebase) para mantener a los usuarios informados.', 'Modificación de pantallas antiguas para optimizar su funcionalidad y diseño.', 'Actualización y modificación de endpoints en el backend, desarrollado en Flask, para mejorar la integración y el rendimiento.']
+    },
+    {
+      empresa: 'Freelance',
+      subtitulo: 'Desarrollo web para un taller mecanico de la zona con Next.js "JARA MOTORS"',
+      acciones: ['Diseño y maquetado con html css y tailwind', 'adpatacion del diseño para todas las pantallas (diseño responsive)', 'creacion de un servicio backend para el envio de correos']
+    },
+    {
+      empresa: 'Freeelance',
+      subtitulo: 'Desarrollo web para un salon de estetica facial de la zona con Next.js "AJ Piel Genuina',
+      acciones: ['Diseño y maquetado con html css y tailwind', 'adpatacion del diseño para todas las pantallas (diseño responsive)', 'creacion de un servicio backend para el envio de correos']
+    }
+  ]
+
+
+  return (
+  
+  <>
+      <header id="cabezera" className="flex items-center justify-center min-h-screen">
+      <h1 className="text-center text-2xl  md:text-6xl text-white font-semibold">Ingeniero Informático/Desarrollador</h1>
+    </header>
+
+    <div className="contenedor">
+   
+
+   <div className="content flex flex-col md:flex-row">
+
+     <div className="flex-1 flex items-center justify-center">
+       <Image
+         src={x}
+         width={350} // Ajusta el tamaño de la imagen
+         height={250} // Ajusta el tamaño de la imagen
+         alt=""
+         className="rounded-full"
+       />
+     </div>
+
+     <div className="about-info flex-1 p-4">
+       <h2 className="text-center text-2xl">Nicolás Rubio</h2>
+       <p className="text-center">
+         Soy ingeniero informático especializado en desarrollo web, apasionado por enfrentar desafíos y encontrar soluciones creativas. Mi formación autodidacta me ha permitido abordar proyectos de manera versátil, desde aplicaciones web complejas hasta interfaces intuitivas. Siempre busco explorar nuevas tecnologías y enfrentar problemas que me permitan seguir creciendo profesionalmente. Estoy abierto a nuevas oportunidades para aplicar mis habilidades y colaborar en proyectos innovadores.
+       </p>
+       <div className="md:flex flex-row justify-center space-x-4">
+         <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" /></svg>
+         <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.3-1 1.6-3.6 0-5.9-1.6-2.3-4.3-3.3-5.6-2.3zm33.5-3.9c1.6 .6 2.9 2.3 2.9 4.2 0 2-1.3 3.9-2.9 4.2-1.6 .3-3.6-1-4.2-2.9-1-2-1.6-3.9-.7-4.2 1.3-.3 3.3 1 4.2 2.9zm15.8-34.4c1.6 1.3 1.6 4.2 0 5.9-1.6 1.6-4.2 1.6-5.9 0-1.6-1.6-1.6-4.2 0-5.9 1.6-1.6 4.2-1.6 5.9 0zm18.3-11.5c1.6 1.3 1.6 4.2 0 5.9-1.6 1.6-4.2 1.6-5.9 0-1.6-1.6-1.6-4.2 0-5.9 1.6-1.6 4.2-1.6 5.9 0zm4.2-32.4c1.6 1.6 1.6 4.3 0 5.9-1.6 1.6-4.3 1.6-5.9 0-1.6-1.6-1.6-4.3 0-5.9 1.6-1.6 4.3-1.6 5.9 0zm13.6 24.7c1.6 1.6 1.6 4.3 0 5.9-1.6 1.6-4.3 1.6-5.9 0-1.6-1.6-1.6-4.3 0-5.9 1.6-1.6 4.3-1.6 5.9 0z" /></svg>
+         <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+       </div>
+     </div>
+   </div>
+
+   <div className="">
+     <h2 className="text-2xl">EXPERIENCIA LABORAL</h2>
+
+     {
+       empleos.map((empleo, index) => (
+         <div key={index} className="mt-16 experience-detail">
+           <h3 className="text-lg font-semibold">{empleo.empresa}</h3>
+           <p className="mt-2 mb-4">{empleo.subtitulo}</p>
+           <ul className="list-disc pl-5 space-y-2">
+             {
+               empleo.acciones.map((acc, index) => (
+                 <li key={index} className="flex items-start space-x-2">
+                   <svg className="flex-shrink-0 w-4 h-4 text-pink-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                   </svg>
+                   <span>{acc}</span>
+                 </li>
+               ))
+             }
+
+
+           </ul>
+         </div>
+
+       ))
+     }
+
+
+   </div>
+
+
+   <div className="content">
+
+
+   </div>
+
+ </div>
+
+
+
+  </>
+
+
+
+
   );
 }
