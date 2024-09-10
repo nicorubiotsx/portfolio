b93b6a56b3feb91b8a9a80e1f411fd18838fa460
nicorubiotsx <nrubioolivares@gmail.com>
2024-09-10 15:53:07 -0300

    portfolio add content 344444

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,7 @@ export default function RootLayout({
       >
         {children}
 
-        <script
-          type="text/javascript"
-          src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js"></script>
+      
       </body>
     </html>
   );
