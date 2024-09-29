a333a2032b4afb6131b5531870cd3c91f864893c
nicorubiotsx <nrubioolivares@gmail.com>
2024-09-29 14:43:49 -0300

    change ok

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
-import { Code,  Github, Linkedin, Phone, MapPin, GraduationCap } from "lucide-react"
-import { Card, CardContent, CardDescription, CardHeader,CardTitle, CardFooter} from "@/components/ui/card";
-import { Accordion ,AccordionItem,AccordionContent,AccordionTrigger} from "@/components/ui/accordion";
+import { Code, Github, Linkedin, Phone, MapPin, GraduationCap } from "lucide-react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
+import { Accordion, AccordionItem, AccordionContent, AccordionTrigger } from "@/components/ui/accordion";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -15,17 +15,17 @@ export default function Home() {
 
 
 
-  
+
 
   interface FormData {
     name: string;
     email: string;
     message: string
   }
-  
-  const [formData, setFormData] = useState<FormData>({ name: '', email: '', message:'' });
 
-  const handleSubmit =async (e: React.FormEvent) => {
+  const [formData, setFormData] = useState<FormData>({ name: '', email: '', message: '' });
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
 
@@ -38,9 +38,9 @@ export default function Home() {
       body: JSON.stringify(formData),
     });
     const result = await response.json();
-    if(response.ok){
-      setFormData({name:'',email:'',message:''})
-     
+    if (response.ok) {
+      setFormData({ name: '', email: '', message: '' })
+
 
     }
     console.log(result);
@@ -48,7 +48,7 @@ export default function Home() {
   }
 
 
-  const handlechange =(e:ChangeEvent <HTMLInputElement | HTMLTextAreaElement> )=>{
+  const handlechange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value })
   }
@@ -90,26 +90,26 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
       {/* Header */}
- 
+
 
       {/* Hero Section */}
       <section className="relative bg-gray-900 text-white py-32">
-  <div
-    className="absolute inset-0 bg-cover bg-center"
-    style={{ backgroundImage: "url('/img/portafolio.png')" }}
-  ></div>
-  <div className="absolute inset-0 bg-black opacity-50"></div>
-  <div className="relative container mx-auto px-4 z-10">
-    <h2 className="text-4xl md:text-5xl font-bold mb-4">Ingeniero de Software</h2>
-    <p className="text-xl mb-8">Creando soluciones innovadoras con código</p>
-    <div className="flex space-x-4">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{ backgroundImage: "url('/img/portafolio.png')" }}
+        ></div>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative container mx-auto px-4 z-10">
+          <h2 className="text-4xl md:text-5xl font-bold mb-4">Ingeniero de Software</h2>
+          <p className="text-xl mb-8">Creando soluciones innovadoras con código</p>
+          <div className="flex space-x-4">
 
 
 
 
-    </div>
-  </div>
-</section>
+          </div>
+        </div>
+      </section>
 
 
       {/* About Me Section */}
@@ -117,12 +117,12 @@ export default function Home() {
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 text-center">Sobre Mí</h2>
           <div className="max-w-3xl mx-auto">
-            <p className="text-lg mb-6">
-             
-Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desarrollo web y aplicaciones móviles. Aunque sigo aprendiendo y mejorando mis habilidades, me esfuerzo por crear soluciones eficientes utilizando tecnologías modernas.
+            <p className="md: text-lg mb-6">
+
+              Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desarrollo web y aplicaciones móviles. Aunque sigo aprendiendo y mejorando mis habilidades, me esfuerzo por crear soluciones eficientes utilizando tecnologías modernas.
             </p>
-            <p className="text-lg mb-6">
-            Me enfoco en escribir código lo más limpio posible y en mejorar la experiencia del usuario, además de optimizar el rendimiento de las aplicaciones cuando es necesario.            </p>
+            <p className=" md:text-lg mb-6">
+              Me enfoco en escribir código lo más limpio posible y en mejorar la experiencia del usuario, además de optimizar el rendimiento de las aplicaciones cuando es necesario.            </p>
 
           </div>
         </div>
@@ -141,9 +141,9 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
                 </CardTitle>
                 <CardDescription>AIEP, 2014-2019</CardDescription>
               </CardHeader>
-          
+
             </Card>
-    
+
             <Card>
               <CardHeader>
                 <CardTitle className="flex items-center">
@@ -155,7 +155,7 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
                 <ul className="list-disc list-inside">
                   <li>React de 0 a experto - Udemy</li>
                   <li>Nodejs-Udemy</li>
-            
+
                 </ul>
               </CardContent>
             </Card>
@@ -193,80 +193,99 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
           <h2 className="text-3xl font-bold mb-8 text-center">Experiencia Laboral</h2>
           <Accordion type="single" collapsible className="w-full">
             {
-              empleos.map((empleo,index)=>(
-                     <AccordionItem key={index} value={`item-${index}`}>
-              <AccordionTrigger>{empleo.empresa}</AccordionTrigger>
-              <AccordionContent>
-                <p className="mb-2">{empleo.subtitulo}</p>
-                <ul className="list-disc list-inside">
-                  <li>{empleo.acciones}</li>
-               
-                </ul>
-              </AccordionContent>
-            </AccordionItem>
+              empleos.map((empleo, index) => (
+                <AccordionItem key={index} value={`item-${index}`}>
+                  <AccordionTrigger className=" md:text-lg">{empleo.empresa}</AccordionTrigger>
+                  <AccordionContent>
+                    <p className="mb-2 text-sm md:text-lg">{empleo.subtitulo}</p>
+                    <ul className="list-disc list-inside">{
+
+                      empleo.acciones.map((accion, index) => (
+                        <li key={index}>{accion}</li>
+                      ))
+
+                    }
+
+
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
 
               ))
             }
-       
-      
-        
+
+
+
           </Accordion>
         </div>
       </section>
 
       {/* Projects Section */}
       <section className="py-16 bg-white" id="proyectos">
-  <div className="container mx-auto px-4">
-    <h2 className="text-3xl font-bold mb-8 text-center">Mis Proyectos</h2>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
- 
-        <Card className="overflow-hidden shadow-lg">
-          <div className="relative h-48 bg-gray-300"> {/* Clase relative añadida */}
-            <Image
-              src={'/img/jaramotors.png'}
-              alt=""
-              fill
-              className="object-cover" 
-            />
-          </div>
-          <CardHeader>
-            <CardTitle>Proyecto</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-sm text-gray-600">
-              Descripción breve del proyecto  Aquí puedes detallar las tecnologías utilizadas y los desafíos superados.
-            </p>
-          </CardContent>
-          <CardFooter>
-          <a href="https://jaramotors.cl"  target="_blank" >jaramotors</a>
-          </CardFooter>
-        </Card>
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center">Mis Proyectos</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
-        <Card className="overflow-hidden shadow-lg">
-          <div className="relative h-48 bg-gray-300"> {/* Clase relative añadida */}
-            <Image
-              src={'/img/ajpielgenuina.png'}
-              alt=""
-              fill
-              className="object-cover" 
-            />
+            <Card className="overflow-hidden shadow-lg">
+              <div className="relative h-48 bg-gray-300"> {/* Clase relative añadida */}
+                <Image
+                  src={'/img/jaramotors.png'}
+                  alt=""
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <CardHeader>
+                <CardTitle>JARAMOTORS</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm text-gray-600 md:text-base">
+      
+Creé un sitio web moderno y eficiente para Jara Motors, un taller mecánico destacado en la industria automotriz. Implementé Next.js y Tailwind CSS para desarrollar una plataforma ágil y completamente optimizada para dispositivos móviles.</p>
+              </CardContent>
+              <CardFooter>
+                <Image
+                  src={'/img/enlace.png'}
+                  width={20}
+                  height={20}
+                  alt=""
+                />
+                <a href="https://jaramotors.cl" target="_blank" >Haz click aqui para visitar</a>
+              </CardFooter>
+            </Card>
+
+            <Card className="overflow-hidden shadow-lg">
+              <div className="relative h-48 bg-gray-300"> {/* Clase relative añadida */}
+                <Image
+                  src={'/img/ajpielgenuina.png'}
+                  alt=""
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <CardHeader>
+                <CardTitle>AJ Piel Genuina</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm text-gray-600  md:text-base">
+
+Este proyecto consistió en crear un sitio web moderno y responsivo para el Centro de Estética Facial Piel Genuina, especializado en el cuidado de la piel. Utilicé Next.js y Tailwind CSS para desarrollar una plataforma atractiva con información detallada sobre los tratamientos, ubicación y contacto del centro.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Image
+                  src={'/img/enlace.png'}
+                  width={20}
+                  height={20}
+                  alt=""
+                />
+                <a href="https://ajpielgenuina.cl" target="_blank" >Haz click aqui para visitar</a>
+              </CardFooter>
+            </Card>
+
           </div>
-          <CardHeader>
-            <CardTitle>Proyecto</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-sm text-gray-600">
-              Descripción breve del proyecto  Aquí puedes detallar las tecnologías utilizadas y los desafíos superados.
-            </p>
-          </CardContent>
-          <CardFooter>
-          <a href="https://ajpielgenuina.cl"  target="_blank" >ajpielgenuina</a>
-          </CardFooter>
-        </Card>
- 
-    </div>
-  </div>
-</section>
+        </div>
+      </section>
 
 
       {/* Contact Form Section */}
@@ -284,7 +303,7 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
                   <div className="space-y-2">
                     <Label htmlFor="name">Nombre</Label>
                     <Input
-                    name="name"
+                      name="name"
                       id="name"
                       placeholder="Tu nombre"
                       value={formData.name}
@@ -302,7 +321,7 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
                       value={formData.email}
                       onChange={handlechange}
                       required
-                      
+
                     />
                   </div>
                   <div className="space-y-2">
@@ -325,7 +344,7 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
                 <CardDescription>Otras formas de ponerte en contacto conmigo.</CardDescription>
               </CardHeader>
               <CardContent className="space-y-4">
-             
+
                 <div className="flex items-center space-x-2">
                   <Phone className="h-5 w-5 text-gray-500" />
                   <a href="tel:+56934640426"> +56934640426</a>
@@ -335,17 +354,17 @@ Hola, soy Nicolás, un ingeniero de software entusiasta con experiencia en desar
                   <span>Las Cabras, Chile</span>
                 </div>
                 <div className="flex space-x-4 mt-4">
-                <Link href="https://github.com/nicorubiotsx" target="_blank">
-  <Button variant="outline" className="bg-white text-gray-900 hover:bg-gray-200">
-    <Github className="mr-2 h-4 w-4" /> GitHub
-  </Button>
-</Link>
-<Link href="https://www.linkedin.com/in/nicolas-rubio/" target="_blank">
-  <Button variant="outline" className="bg-white text-gray-900 hover:bg-gray-200">
-    <Linkedin className="mr-2 h-4 w-4" /> LinkedIn
-  </Button>
-</Link>
-             
+                  <Link href="https://github.com/nicorubiotsx" target="_blank">
+                    <Button variant="outline" className="bg-white text-gray-900 hover:bg-gray-200">
+                      <Github className="mr-2 h-4 w-4" /> GitHub
+                    </Button>
+                  </Link>
+                  <Link href="https://www.linkedin.com/in/nicolas-rubio/" target="_blank">
+                    <Button variant="outline" className="bg-white text-gray-900 hover:bg-gray-200">
+                      <Linkedin className="mr-2 h-4 w-4" /> LinkedIn
+                    </Button>
+                  </Link>
+
                 </div>
               </CardContent>
             </Card>
